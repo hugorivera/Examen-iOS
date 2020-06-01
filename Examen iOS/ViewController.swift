f9a2bb80cfd9597dac8fb05308dfaca447de901e
hugorivera <riveraquevedoh@gmail.com>
2020-05-31 23:03:24 -0500

    Initial Commit

---
+++ Examen iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examen iOS
+//
+//  Created by Alio Mexico on 31/05/20.
+//  Copyright © 2020 Hugo Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
